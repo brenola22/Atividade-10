f087a621632b5bb534c9fb0363481f3347cb65b6
brenola22 <brenosantosgonzaga0@gmail.com>
2026-08-26 17:12:35 -0300

    inicio java

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 let nome = prompt("Digite seu nome: ")
 let sobrenome = prompt("Digite seu sobrenome: ")
-let data = new Date().toLocaleString("pt-BR", { dateStyle: "full", timeStyle: "short" })
+let data = new Date().toLocaleString("pt-BR", {dateStyle: "short", timeStyle: "short", hour12: false })
 console.log(`Olá ${nome} ${sobrenome} hoje é ${data}. 67`)
+
+
+setInterval(() => {
+    let bemVindoElement = document.getElementById("bem-vindo")
+    bemVindoElement.innerHTML = `Bem-vindo, ${nome} ${sobrenome}! Hoje é ${data}.`
+}, 3*1000)
+
